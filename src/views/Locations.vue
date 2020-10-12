4da0761d8e8d30d7b07437cf14aa2480b761e264
michael beloff <michael.a.beloff@gmail.com>
2020-10-13 08:12:41 +1000

    Update Locations.vue

--- src/views/Locations.vue
+++ src/views/Locations.vue
@@ -3,8 +3,8 @@
     <div class="container py-5">
       <Title>Locations</Title>
       <p>
-        Verified Autoshop is expanding and currently has locations in Australia
-        and New Zealand.
+        Verified Autoshop is expanding and currently has locations in Brisbane,
+        Australia and Christchuch, New Zealand.
       </p>
     </div>
     <LocationInfo></LocationInfo>
