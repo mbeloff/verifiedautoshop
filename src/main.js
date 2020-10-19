f1bd970d0bc66776c191378a774970a462deadbf
michael beloff <michael.a.beloff@gmail.com>
2020-10-19 14:20:40 +1000

    remove vue-analytics, use with vue-gtm instead

--- src/main.js
+++ src/main.js
@@ -7,9 +7,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import VueNavigationBar from "vue-navigation-bar";
 Vue.component("vue-navigation-bar", VueNavigationBar);
 import VModal from "vue-js-modal";
-import VueAnalytics from "vue-analytics";
+// import VueAnalytics from "vue-analytics";
 import upperFirst from "lodash/upperFirst";
 import camelCase from "lodash/camelCase";
+import VueGtm from "vue-gtm";
 Vue.use(VModal);
 Vue.config.productionTip = false;
 
@@ -47,10 +48,20 @@ requireComponent.keys().forEach(fileName => {
   );
 });
 
-Vue.use(VueAnalytics, {
-  id: "UA-179418961-1",
-  router
+Vue.use(VueGtm, {
+  id: "GTM-58FJJZN",
+  defer: false, // defaults to false. Script can be set to `defer` to increase page-load-time at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible)
+  enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
+  debug: true, // Whether or not display console logs debugs (optional)
+  loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
+  vueRouter: router, // Pass the router instance to automatically sync with router (optional)
+  trackOnNextTick: false // Whether or not call trackView in Vue.nextTick
 });
+
+// Vue.use(VueAnalytics, {
+//   id: "UA-179418961-1",
+//   router
+// });
 
 new Vue({
   router,
