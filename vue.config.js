4fa9cb1c6700afb4f8c45a167f5e62086e56fe2f
michael beloff <michael.a.beloff@gmail.com>
2020-10-20 13:18:45 +1000

    add webpack sitemap plugin

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,47 @@
-const ImageminWebpWebpackPlugin = require("imagemin-webp-webpack-plugin");
+// const ImageminWebpWebpackPlugin = require("imagemin-webp-webpack-plugin");
+const SitemapPlugin = require("sitemap-webpack-plugin").default;
+const paths = [
+  {
+    path: "/",
+    priority: "0.8"
+  },
+  {
+    path: "/quote"
+  },
+  {
+    path: "/locations",
+    priority: "0.6"
+  },
+  {
+    path: "/services",
+    priority: "0.7"
+  },
+  {
+    path: "/contact",
+    priority: "0.6"
+  }
+];
 module.exports = {
   configureWebpack: {
     plugins: [
-      new ImageminWebpWebpackPlugin({
-        config: [
-          {
-            test: /\.(jpe?g|png)/,
-            options: {
-              quality: 80
-            }
-          }
-        ],
-        overrideExtension: true,
-        detailedLogs: false,
-        silent: false,
-        strict: true
+      // new ImageminWebpWebpackPlugin({
+      //   config: [
+      //     {
+      //       test: /\.(jpe?g|png)/,
+      //       options: {
+      //         quality: 80
+      //       }
+      //     }
+      //   ],
+      //   overrideExtension: true,
+      //   detailedLogs: false,
+      //   silent: false,
+      //   strict: true
+      // }),
+      new SitemapPlugin("https://verifiedautoshop.com", paths, {
+        lastmod: true,
+        changefreq: "monthly",
+        priority: "0.5"
       })
     ]
   },
