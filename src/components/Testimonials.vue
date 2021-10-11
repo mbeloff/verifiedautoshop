b926be070303c3c3e94a15a66603979d0a7b7610
michael beloff <michael.a.beloff@gmail.com>
2021-10-11 14:30:58 +1000

    add aggregate  review scores

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,7 +1,45 @@
 <template>
   <div class="light-bg py-4 mt-5">
+    <!-- <div class="w-100 text-center">
+      <a class="inline-block mx-auto" title="review us on Google" href="">
+        <img class="mx-auto rounded-xl" src="@/assets/reviewUs.svg" alt="" />
+      </a>
+    </div> -->
     <div class="mx-auto" style="overflow: hidden; max-width: 100vw">
       <div class="container">
+        <div class="row px-2">
+          <div
+            class="col-12 col-md-5 mb-2 mx-auto"
+            v-for="(item, i) in aggregates"
+            :key="i"
+          >
+            <div class="row">
+              <a class="inline-block" :title="item.title" :href="item.link">
+                <img style="border-radius: 1rem;" :src="item.img" alt="" />
+              </a>
+              <div class="d-flex flex-column justify-content-center pl-2">
+                <p class="mb-0">
+                  <i
+                    class="fas fa-star"
+                    v-for="star in Math.floor(item.stars)"
+                    :key="star"
+                  ></i>
+                  <i class="fas fa-star-half" v-if="item.stars % 1"></i>
+                </p>
+                <p class="mb-0">
+                  <span>{{ item.stars }}</span>
+                  on
+                  {{ item.site }}
+                </p>
+                <p class="mb-0 text-info">
+                  <small>{{ item.reviews }} reviews</small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container mt-5">
         <Title>Testimonials</Title>
       </div>
       <div class="container p-0">
@@ -114,7 +152,36 @@ export default {
           prevEl: ".swiper-button-prev"
         }
       },
+      aggregates: [
+        {
+          site: "AutoGuru",
+          img:
+            "https://res.cloudinary.com/dg5ybbkbh/image/upload/c_scale,w_120/v1633922651/verified/ag-logo-600.png",
+          title: "Verified Autoshop on AutoGuru",
+          link: "https://www.autoguru.com.au/s/verified-autoshop",
+          stars: 4.7,
+          reviews: 14
+        },
+        {
+          site: "Google",
+          img:
+            "https://res.cloudinary.com/dg5ybbkbh/image/upload/c_scale,w_120/v1633922651/verified/review-us-google-600.png",
+          title: "review us on Google",
+          link: "https://g.page/r/CSVrYEbzM1SSEAg/review",
+          stars: 5,
+          reviews: 4
+        }
+      ],
       testimonials: [
+        {
+          author: "Nick L",
+          initial: "N",
+          // img:
+          // "https://res.cloudinary.com/dg5ybbkbh/image/upload/c_fill,h_100,w_100/v1600665252/verified/1.webp",
+          comment:
+            "top workmanship an really good pricing. will be back cheers",
+          stars: 5
+        },
         {
           author: "Serena",
           initial: "S",
@@ -130,25 +197,47 @@ export default {
           stars: 5
         },
         {
-          author: "Tasman Emtage",
+          author: "Tasman E",
           initial: "T",
           comment: "",
           stars: 5
         },
+        {
+          author: "Finnian",
+          initial: "F",
+          comment:
+            "Friendly, helpful, quick and affordable! I only have good things to say about Verified Autoshop and I would strongly recommend them.",
+          stars: 5
+        },
+        {
+          author: "Ana",
+          initial: "A",
+          comment:
+            "The service was friendly. Happy with my new tyres, I will come back!",
+          stars: 5
+        },
+        {
+          author: "Vito",
+          initial: "V",
+          comment:
+            "The guys at the verified auto shop were fantastic! I had a service, tires, and brakes done there, and the job was done well and with quality parts, at a very reasonable price. Communication was excellent, I knew exactly what was going on every step of the way, which is quite rare in my experience. I highly recommend Verified Autoshop for all your mechanical work!",
+          stars: 5
+        },
 
         {
-          author: "Liz",
+          author: "Liz R",
           img:
             "https://res.cloudinary.com/dg5ybbkbh/image/upload/c_fill,h_100,w_100/v1600665252/verified/3.webp",
           comment:
             "I was able to book a service the next day, very professional."
         },
         {
-          author: "Anna",
+          author: "Anna N",
           initial: "A",
           // img:
           //   "https://res.cloudinary.com/dg5ybbkbh/image/upload/c_fill,h_100,w_100/v1600665252/verified/4.webp",
-          comment: "I'm so happy with the service and the price."
+          // comment: "I was happy with the service and the price."
+          stars: 5
         },
         {
           author: "Waz",
